c46caa236a58376ccbe20b244b324a80fdbd1675
Mihir6365 <savla.mp@somaiya.edu>
2023-01-19 14:32:26 +0530

    test..

--- app.js
+++ app.js
@@ -5,22 +5,26 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const client = new MongoClient(process.env.uri);
+const profile_collection = client.db("Teams_summarizer")
+    .collection("User_profiles");
+
+profile_collection.createIndex({ "email": 1 }, { unique: true })
+////////////////////////////////////////////////////  ALL PATHS //////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
-const prfile_collection = client.db("Teams_summarizer")
-    .collection("User_profiles");
+
 app.post('/signup', async (req, res) => {
     let user = req.body;
-    await prfile_collection
+
+    await profile_collection
         .insertOne(user)
     return res.json({ status: true })
 });
 
 app.post('/login', async (req, res) => {
     let user = req.body;
-    client.db("Teams_summarizer")
-        .collection("User_profiles")
+    profile_collection
         .findOne({ email: user.email, password: user.password }, function (err, result) {
             if (err) return { status: false };
             return res.json({ status: true, result: result })
@@ -28,8 +32,7 @@ app.post('/login', async (req, res) => {
 });
 
 app.post('/profile', async (req, res) => {
-    client.db("Teams_summarizer")
-        .collection("User_profiles")
+    profile_collection
         .findOne({ _id: ObjectId(req.body.id) }, function (err, result) {
             if (err) return { status: false };
             return res.json({ status: true, result: result })
