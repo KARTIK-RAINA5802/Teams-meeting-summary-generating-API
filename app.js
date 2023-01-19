d87697883d0ed8cb93e0d098b9d6df8cba24dde5
Mihir6365 <savla.mp@somaiya.edu>
2023-01-19 14:16:41 +0530

    final?

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.post('/signup', async (req, res) => {
     let user = req.body;
     await client.db("Teams_summarizer")
         .collection("User_profiles")
-        .insertOne(user).result(console.log(result))
+        .insertOne(user)
     return res.json({ status: true })
 });
 
@@ -28,12 +28,12 @@ app.post('/login', async (req, res) => {
 });
 
 app.post('/profile', async (req, res) => {
-    var query = { _id: ObjectId(req.body.id) };
     client.db("Teams_summarizer")
         .collection("User_profiles")
-        .find(query).toArray(function (err, result) {
+        .findOne({ _id: ObjectId(req.body.id) }, function (err, result) {
             if (err) return { status: false };
-            return res.json(result)
+            return res.json({ status: true, result: result })
         });
 });
+
 app.listen(process.env.PORT || 3000, () => { console.log("running...") })
