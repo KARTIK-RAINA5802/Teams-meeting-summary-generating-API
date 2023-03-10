799de1cf646b2efe5b4a577a6c076e1e5ff8cb32
Kartik Raina <96180683+KARTIK-RAINA5802@users.noreply.github.com>
2023-03-10 10:01:04 +0530

    Added your uri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = "mongodb://localhost:27017/deepblue";
+const mongoUri = "mongodb+srv://Mihir6365:X9%2Fz1209636599@deep-blue.jmwssa1.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
     mongoose.connect(mongoUri)
